4d018d02dada797557e00581ebad9689d4d30615
Zamiul Hasan <zhfahan77@gmail.com>
2023-10-18 00:24:42 +0600

    feat: ignore query params

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 import json
 from app import app, redis_client
+from app.lib.url_parser import remove_query_params_and_fragment
 from app.scapper.page_scraper import scrape_url
 from app.scapper.pdf_scraper import scrape_pdf
 
@@ -9,17 +10,18 @@ def scrape_page():
     try:
         url = request.json.get('url')
         reCache = request.json.get('reCache')
+        key = remove_query_params_and_fragment(url)
 
         if not url:
                 return jsonify({'error': 'URL not provided'}), 400
 
         if not reCache:
-            cached_data = redis_client.get(url)
+            cached_data = redis_client.get(key)
             if cached_data:
                 return json.loads(cached_data)
 
         data = scrape_url(url)
-        redis_client.set(url, json.dumps(data), ex=3600)
+        redis_client.set(key, json.dumps(data), ex=3600)
         return jsonify(data)
 
     except Exception as e:
@@ -30,17 +32,18 @@ def scrape_pdf_url():
     try:
         url = request.json.get('url')
         reCache = request.json.get('reCache')
+        key = remove_query_params_and_fragment(url)
 
         if not url:
             return jsonify({'error': 'URL not provided'}), 400
 
         if not reCache:
-            cached_data = redis_client.get(url)
+            cached_data = redis_client.get(key)
             if cached_data:
                 return json.loads(cached_data)
 
         data = scrape_pdf(url)
-        redis_client.set(url, json.dumps(data), ex=3600)
+        redis_client.set(key, json.dumps(data), ex=3600)
         return jsonify(data)
 
     except Exception as e:
